ad358385f4eb488d26784459ece529279c628ad8
Keshava Varma <keshavavarma.ns@gmail.com>
2021-09-14 13:05:43 +0530

    added test routes

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const userRouter = require("./routes/api/users");
+const authRouter = require("./routes/api/auth");
+const profileRouter = require("./routes/api/profile");
+const postsRouter = require("./routes/api/posts");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+// api routes
+
+app.use("/api/users", userRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
