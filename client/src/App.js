169e3a5fa2e37db534b99020a9b2a67a33a7aae9
Keshava Varma <keshavavarma.ns@gmail.com>
2021-11-18 13:30:01 +0530

    code splitting

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,56 @@
 import "./App.css";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./screens/Home";
-import Login from "./screens/Login";
-import Register from "./screens/Register";
-import Profile from "./screens/Profile";
-import CreatePost from "./screens/CreatePost";
-import EditProfile from "./screens/EditProfile";
-import OthersProfile from "./screens/OthersProfile";
-import ProtectedRoute from "./components/ProtectedRoute";
+// import Home from "./screens/Home";
+// import Login from "./screens/Login";
+// import Register from "./screens/Register";
+// import Profile from "./screens/Profile";
+// import CreatePost from "./screens/CreatePost";
+// import EditProfile from "./screens/EditProfile";
+// import OthersProfile from "./screens/OthersProfile";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import ViewPost from "./screens/ViewPost";
 import { AuthProvider } from "./contexts/AuthContext";
-import ViewPost from "./screens/ViewPost";
+const Home = lazy(() => import("./screens/Home"));
+const Login = lazy(() => import("./screens/Login"));
+const Register = lazy(() => import("./screens/Register"));
+const Profile = lazy(() => import("./screens/Profile"));
+const CreatePost = lazy(() => import("./screens/CreatePost"));
+const EditProfile = lazy(() => import("./screens/EditProfile"));
+const OthersProfile = lazy(() => import("./screens/OthersProfile"));
+const ProtectedRoute = lazy(() => import("./components/ProtectedRoute"));
+const ViewPost = lazy(() => import("./screens/ViewPost"));
 function App() {
   return (
     <div className="App">
       <Router>
         <AuthProvider>
-          <Switch>
-            <ProtectedRoute exact path="/" component={Home} />
-            <ProtectedRoute path="/Profile">
-              <Profile />
-            </ProtectedRoute>
-            <ProtectedRoute path="/CreatePost">
-              <CreatePost />
-            </ProtectedRoute>
-            <ProtectedRoute path="/ViewPost/:postID">
-              <ViewPost />
-            </ProtectedRoute>
-            <ProtectedRoute path="/EditProfile">
-              <EditProfile />
-            </ProtectedRoute>
-            <Route path="/user/:userID">
-              <OthersProfile />
-            </Route>
-            <Route path="/Login">
-              <Login />
-            </Route>
-            <Route path="/Register">
-              <Register />
-            </Route>
-          </Switch>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Switch>
+              <ProtectedRoute exact path="/" component={Home} />
+              <ProtectedRoute path="/Profile">
+                <Profile />
+              </ProtectedRoute>
+              <ProtectedRoute path="/CreatePost">
+                <CreatePost />
+              </ProtectedRoute>
+              <ProtectedRoute path="/ViewPost/:postID">
+                <ViewPost />
+              </ProtectedRoute>
+              <ProtectedRoute path="/EditProfile">
+                <EditProfile />
+              </ProtectedRoute>
+              <Route path="/user/:userID">
+                <OthersProfile />
+              </Route>
+              <Route path="/Login">
+                <Login />
+              </Route>
+              <Route path="/Register">
+                <Register />
+              </Route>
+            </Switch>
+          </Suspense>
         </AuthProvider>
       </Router>
     </div>
